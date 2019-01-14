099c69103a3b460e5fcb346c1231a9d4a1c4a4a9
Angel Nunez Mencias <git@angelnu.com>
2019-01-14 02:13:55 +0100

    Update common.js

--- examples/common.js
+++ examples/common.js
@@ -8,27 +8,31 @@ module.exports = function(log) {
   let exports = {};
 
   var playAndCheckPromise = function (mediaPlayer, url, url2Check) {
-    
+
     return mediaPlayer.playUrlPromise(url)
     .then( function() {
-      return new Promise(function (resolve, reject) {
-        mediaPlayer.once(mediaPlayer.EVENT_PLAYER_PLAYING, function(contentId){
-          log.info("PLAYING: %s", contentId);
-          if (url2Check && url2Check != contentId)
-            reject();
-          else
-            resolve(mediaPlayer);
+      if (mediaPlayer.getPlayerStatus().playerState == "PLAYING")
+        return mediaPlayer;
+      else
+        return new Promise(function (resolve, reject) {
+          mediaPlayer.once(mediaPlayer.EVENT_PLAYER_PLAYING, function(contentId){
+            log.info("PLAYING: %s", contentId);
+            if (url2Check && url2Check != contentId)
+              reject();
+            else
+              resolve(mediaPlayer);
+          });
+          //TBD: add timeout check
         });
-        //TBD: add timeout check
-      });
     });
   }
   exports.playAndCheckPromise = playAndCheckPromise;
-  
+
   var setupPromise = function(){
     let mediaPlayer;
-  
+
     //Setup
+    log.info("Scan for : %s", defines.deviceName);
     return ScannerPromise(defines.deviceName)
     .then (function (device) {
       mediaPlayer = new MediaPlayer(device);
@@ -42,21 +46,26 @@ module.exports = function(log) {
     });
   }
   exports.setupPromise = setupPromise;
-  
+
   var finalizeOk = function(mediaPlayer){
     log.info("ALL URLs played fine");
-    return Promise.resolve();  
+    mediaPlayer.close();
+    return Promise.resolve();
   }
   exports.finalizeOk = finalizeOk;
-  
+
   var finalizeError = function(mediaPlayer, err){
     log.error("ERROR");
+    log.error("ERROR : %s", err.stack);
+
+    mediaPlayer.close();
+
     if (err === undefined)
       err = Error("undefined");
-    log.error("ERROR : %s", err.stack);
-    return Promise.reject(err);  
+    return Promise.reject(err);
+
   }
   exports.finalizeError = finalizeError;
-  
+
   return exports;
 }
